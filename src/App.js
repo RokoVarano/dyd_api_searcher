6582a75543c245ad735a315352fc7fea2a67c1db
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-09-06 14:52:21 -0300

    setup templates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
 
 function App() {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(fetchRockets());
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Header /> */}
+      <Switch>
+        {/* <Route path="/" exact component={Home} /> */}
+      </Switch>
+    </Router>
   );
 }
 
