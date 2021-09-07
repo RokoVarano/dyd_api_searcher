0534d0d309e25cfe60ad1176c0dd96cacea4a739
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-09-07 19:26:50 -0300

    Refactor to switch for subgroups

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link, useRouteMatch } from 'react-router-dom';
+import {
+  Link, Route, useRouteMatch, Switch,
+} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import capFirst from '../../utilities';
 
 const List = (props) => {
-  const { url } = useRouteMatch();
+  const { url, path } = useRouteMatch();
   const { categories } = props;
 
   const categoriesJSX = (categories) => categories.map((category) => {
@@ -13,11 +15,15 @@ const List = (props) => {
     const count = category.category ? <p>{`${category.category.count} items`}</p> : null;
 
     return (
-
-      <Link to={`${address}`} key={name}>
-        <h3>{capFirst(name)}</h3>
-        { count }
-      </Link>
+      <Switch key={name}>
+        <Route exact path={path}>
+          <Link to={`${address}`} key={name}>
+            <h3>{capFirst(name)}</h3>
+            { count }
+          </Link>
+        </Route>
+        <Route path={`${path}/${category.index}`}>{category.url}</Route>
+      </Switch>
 
     );
   });
---
+++ src/redux/details.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const GET_DETAILS = 'GET_DETAILS';
+
+const baseUrl = 'https://www.dnd5eapi.co';
+
+const getDetails = (url) => async (dispatch) => {
+  axios.get(`${baseUrl + url}`).then((res) => dispatch({ type: GET_DETAILS, data: Object.entries(res.data) }));
+};
+
+const details = (state = [], action) => {
+  switch (action.type) {
+    case GET_DETAILS:
+      return action.data;
+    default: return state;
+  }
+};
+
+export default details;
+export { getDetails };
