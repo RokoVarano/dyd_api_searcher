71501f54e592085f5c7f8218e413fd712b0fa27a
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-09-08 17:12:48 -0300

    Details style

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDetails } from '../../redux/details';
+import capFirst from '../../utilities';
 
 const Details = (props) => {
   const { url } = props;
@@ -13,12 +14,31 @@ const Details = (props) => {
 
   const details = useSelector((state) => state.details);
 
-  const populate = (data) => Object.entries(data).map((pair) => (
-    <div key={pair[0]}>
-      <h2>{pair[0]}</h2>
-      {typeof pair[1] === 'string' ? pair[1] : populate(pair[1])}
-    </div>
-  ));
+  const populate = (data) => Object.entries(data).map((pair) => {
+    if (pair[1] === null) return null;
+
+    if (pair[0] === 'name') {
+      return (
+        <div key={pair[0]} className="details-item">
+          <h3>
+            <i className="fas fa-dragon" />
+            {capFirst(pair[1])}
+          </h3>
+        </div>
+      );
+    }
+
+    if (!['index', 'url'].includes(pair[0])) {
+      return (
+        <div key={pair[0]} className={`details-item ${['array', 'object'].includes(typeof pair[1]) || ['desc'].includes(pair[0]) ? 'border' : 'row'}`}>
+          {Number.isNaN(Number(pair[0])) ? <h3>{capFirst(pair[0].replace('_', ' '))}</h3> : null}
+          {['string', 'number'].includes(typeof pair[1]) ? <p className={pair[1].length > 10 ? '' : 'details-item-value'}>{pair[1]}</p> : populate(pair[1])}
+        </div>
+      );
+    }
+
+    return null;
+  });
 
   return (
     <div className="details">
